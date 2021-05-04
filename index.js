1017a0484aa2c97dc4c3681fe9dfe8d671cc636e
Maksym Zyza <mmziza88@gmail.com>
2021-05-04 14:36:39 +0300

    видалено закоментований текст

--- index.js
+++ index.js
@@ -32,18 +32,3 @@ function invokeAction({ action, id, name, email, phone }) {
 }
 
 invokeAction(argv);
-
-// ========== commander
-// const { Command } = require("commander");
-// const program = new Command();
-
-// program
-//   .option("-a, --action <type>", "choose action")
-//   .option("-i, --id <type>", "user id")
-//   .option("-n, --name <type>", "user name")
-//   .option("-e, --email <type>", "user email")
-//   .option("-p, --phone <type>", "user phone");
-
-// program.parse(process.argv);
-
-// const argv = program.opts();
